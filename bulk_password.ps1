391d69b3b6009ae267fd8cc15ecb83a0686f8023
Joe Rodriguez <joerod@gmail.com>
2013-08-06 22:57:50 -0400

    Create bulk_password.ps1
    This will take a csv with a column of email addresses and reset the password to a random 8 character alpha-numeric string.  It will also log all events to a txt file. I use this script in conjunction with GAM (

---
+++ bulk_password.ps1
@@ -0,0 +1,29 @@
+#used for output trascript
+$ErrorActionPreference="SilentlyContinue"
+Stop-Transcript | out-null
+$ErrorActionPreference = "Continue"
+
+#set this to the location of the gam binaries
+Set-Alias gam C:\gam\gam.exe
+
+#Imports data from csv, mandatory column 1 (email) address
+$users = Import-Csv C:\scripts\users.csv
+
+
+Start-Transcript -path C:\scripts\password_output.txt -append
+
+foreach($column in $users)
+{
+ clv pass
+ #creates random 8 charecter password
+ $r = New-Object System.Random
+ 1..8 | % { $pass += [char]$r.next(33,126) }
+ 
+ gam update user $($column.email) password $pass
+
+ Write-Host "$($column.email) password changed $pass" 
+ clv pass
+ }
+
+Stop-Transcript
+
