36a051bf2d2b8f89cd3af2cdce7432cd27bfdfd3
Joe Rodriguez <joerod@gmail.com>
2013-10-04 12:05:32 -0400

    Create remove_null_groups.ps1

---
+++ remove_null_groups.ps1
@@ -0,0 +1,23 @@
+ #This script finds empty groups in the OU listed in the 4th line
+
+ function empty_groups{
+ Get-ADGroup -Filter {GroupCategory -eq 'Security'} -SearchBase ",OU=Empty,DC=contoso, DC=local" `
+ | ?{@(Get-ADGroupMember $_).Length -eq 0} `
+ | Select-Object Distinguishedname
+ }
+
+foreach($i in empty_groups){
+#removes characters from beginning and end of the empty_ou function 
+$i = $i -replace "@{Distinguishedname=", ""
+$i = $i -replace "}", ""
+
+#shows progress of script
+Write-Host $i
+
+#removes accidental deletion of object
+Set-ADObject $i -ProtectedFromAccidentalDeletion $false
+
+#removes the object from AD
+Remove-ADGroup -Identity $i -confirm:$false
+
+}
