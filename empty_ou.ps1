678385079e0939a94bf0427f25c01a1830b5b0d9
Joe Rodriguez <joerod@gmail.com>
2013-10-04 11:09:58 -0400

    Update empty_ou.ps1

--- empty_ou.ps1
+++ empty_ou.ps1
@@ -6,4 +6,20 @@ Get-ADOrganizationalUnit -Filter * -SearchBase "OU=Empty,DC=contoso, DC=local" `
 | Select-Object Distinguishedname
 }
 
-empty_OU
+#this removes each empty OU
+foreach($i in empty_OU){
+#removes characters from beginning and end of the empty_ou function 
+$i = $i -replace "@{Distinguishedname=", ""
+$i = $i -replace "}", ""
+
+#shows progress of script
+Write-Host "Removing $i"
+
+#removes accidental deletion of object
+Set-ADObject $i -ProtectedFromAccidentalDeletion $false
+
+#removes the object from AD
+Remove-ADOrganizationalUnit -Identity $i -confirm:$false
+
+}
+
