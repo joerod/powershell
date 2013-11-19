6eea74814f1f252ed17ab07d04fcf62dc82e674f
Joe Rodriguez <joerod@gmail.com>
2013-11-19 09:47:40 -0500

    Update Mirror_AD_Groups.ps1

--- Mirror_AD_Groups.ps1
+++ Mirror_AD_Groups.ps1
@@ -1,6 +1,14 @@
-$strUserName = Read-Host 'Enter username'
-$newuser = Read-Host 'Copy groups to this user'
-$groupname = get-aduser $strUserName -Properties memberOF |Select-Object memberOF -ExpandProperty memberOF
+#Added a Param to make it easier to run this script user -CopyUser and -ToUser
+Param(
+[String]$CopyUser,
+[String]$ToUser
+)
+Function Copy-AdGroups{
+$groupname = get-aduser $CopyUser -Properties memberOF |Select-Object memberOF -ExpandProperty memberOF
 foreach($i in $groupname){
-Add-ADPrincipalGroupMembership -Identity $newuser -MemberOf $i
+Add-ADPrincipalGroupMembership -Identity $ToUser -MemberOf $i
+Write-Host "Copied $i to $ToUser"
 }
+
+}
+Copy-AdGroups
