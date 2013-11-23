780ac2f80980874988a8c661d17f1991b658646a
Joe Rodriguez <joerod@gmail.com>
2013-11-23 00:30:00 -0500

    Create file_migration.ps1
    Prerequisite robocopy
                 RSAT
    
    This script will copy files from one server (server_a) to another (server_b) using robocopy and a list of folders.
    First we check to see if the folder being copied to the destination server (server_b) exists on the host server (server_a), 
    if not  the folder is created and  the ACL are modified, Then the users AD profile is updated with the new home drive and specified drive letter.  
    
    If the folder already exists on server_a robocopy copies all the contents including file perms and sends a log everything

---
+++ file_migration.ps1
@@ -0,0 +1,42 @@
+<# Prerequisite robocopy
+                RSAT
+
+This script will copy files from one server (server_a) to another (server_b) using robocopy and a list of folders.
+First we check to see if the folder being copied to the destination server (server_b) exists on the host server (server_a), 
+if not  the folder is created and  the ACL are modified, Then the users AD profile is updated with the new home drive and specified drive letter.  
+
+If the folder already exists on server_a robocopy copies all the contents including file perms and sends a log everything
+#>
+
+
+$users = Get-Content C:\Users\admin\Desktop\userlist.txt
+
+foreach($user in $users){
+
+#checks if user has a home directory if not it creates one on target server
+if((Test-Path -Path "\\server_a\d$\user\$user") -eq $false) {
+#creates new path since one does not exist
+$CreatePath = New-Item -Path "\\server_b\f$\UsersNY\$user" -itemtype directory
+$CreatePath = "\\server_b\f$\UserNew\$user"
+
+#sets file permsion on new folder
+$acl = Get-Acl $CreatePath
+$permission = "contoso\$user","Modify", "ContainerInherit, ObjectInherit", "None", "Allow"
+$accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
+$acl.SetAccessRule($accessRule)
+$acl | Set-Acl $CreatePath
+
+#adds new home directory to AD
+$drive = "\\contoso\dfs\UsersNew\"
+Set-ADUser $user -HomeDirectory ("$drive" + "$user") -HomeDrive U 
+
+  }
+  else
+  {
+#Copies data (only delta if file already exist)
+robocopy \\server_a\f$\UsersOld\$user \\server_b\f$\UK_Users\$user /e /COPYALL /xo /Z /R:1000000 /W:1 /LOG+:C:\robocopy.log /tee
+#Sets home drive in AD
+$drive = "\\contoso\dfs\UsersNew\"
+Set-ADUser $user -HomeDirectory ("$drive" + "$user") -HomeDrive U 
+  }
+}
