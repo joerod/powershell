ef1cf5fffb56dab9cb34d88efa8b6780fa384741
Joe Rodriguez <joerod@gmail.com>
2013-12-13 01:50:30 -0500

    Update remove_null_groups.ps1

--- remove_null_groups.ps1
+++ remove_null_groups.ps1
@@ -3,13 +3,10 @@
  function empty_groups{
  Get-ADGroup -Filter {GroupCategory -eq 'Security'} -SearchBase "OU=Empty,DC=contoso, DC=local" `
  | ?{@(Get-ADGroupMember $_).Length -eq 0} `
- | Select-Object Distinguishedname
+ | Select-Object Distinguishedname -expandproperty Distinguishedname 
  }
 
 foreach($i in empty_groups){
-#removes characters from beginning and end of the empty_ou function 
-$i = $i -replace "@{Distinguishedname=", ""
-$i = $i -replace "}", ""
 
 #shows progress of script
 Write-Host $i
