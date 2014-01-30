8b8ca9c05bad921d574d17a42eb539c4d29c506b
Joe Rodriguez <joerod@gmail.com>
2014-01-30 10:31:48 -0500

    Update remove_dns.ps1

--- remove_dns.ps1
+++ remove_dns.ps1
@@ -1,6 +1,4 @@
 #This script reads off a csv with column 1 being a list of IPs and column 2 the corresponding computer names.  
-
-
 ForEach($ip in (Import-Csv C:\temp\removedns.csv){
 
 dnscmd dnsserver /RecordDelete contoso.local $ip.name A $ip.ip /f
