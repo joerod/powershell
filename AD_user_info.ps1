cfab2ec12633b8b92746665da86d155ebbcfb47d
Joe Rodriguez <joerod@gmail.com>
2014-01-30 14:42:15 -0500

    Update AD_user_info.ps1

--- AD_user_info.ps1
+++ AD_user_info.ps1
@@ -3,10 +3,10 @@
 
 $searchBase = 'ou=users,ou=resources,dc=contoso,dc=local'
  
-$users = Get-ADUser -filter "mail -like '*@contoso.com'" -SearchBase $searchBase 
+$users = Get-ADUser -filter {mail -like '*@contoso.com'} -SearchBase $searchBase 
 
 Foreach ($user in $users){ 
 Get-ADUser -Identity $user -Properties Displayname, Mail, StreetAddress, OfficePhone, MobilePhone `
-|select Displayname, Mail, StreetAddress, OfficePhone, MobilePhone `
-|Export-Csv -Path C:\Users\admin\Desktop\contoso_users.csv -Append
+|select Displayname, Mail, StreetAddress, OfficePhone, MobilePhone |
+ Export-Csv -Path C:\Users\admin\Desktop\contoso_users.csv -Append
 }
