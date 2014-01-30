dd10544844ae628fc80e73539ce1779b24fd6400
Joe Rodriguez <joerod@gmail.com>
2014-01-30 14:22:29 -0500

    Update optional_features.ps1

--- optional_features.ps1
+++ optional_features.ps1
@@ -1,9 +1,8 @@
-
 Param(
 [Parameter(Position=0,mandatory=$true)]
 [string]$Hostname
 )
 
  gwmi win32_OptionalFeature -computername $Hostname `
- | Where-Object {$_.installstate -eq 1} | Select-Object Caption |sort-object caption
+ | Where-Object {$_.installstate -eq 1} | Select-Object Caption |Sort caption
  
