028c6730b5a9e49fed11e51e7e415bab6862ef04
Joe Rodriguez <joerod@gmail.com>
2014-02-07 11:17:35 -0500

    Update samaccount_from_real_name.ps1
    Added -All switch to toggle between enabled and disabled users in results

--- samaccount_from_real_name.ps1
+++ samaccount_from_real_name.ps1
@@ -1,7 +1,18 @@
 #find users samaccount name from real name
+#-all switch shows every user in AD even if the account is not enabled.  
 param
     (
-        [Parameter(Mandatory=$true)] [string]$Name
-      )
+        # Param1 help description
+        [Parameter(Mandatory=$true)] [string]$Name,
+        [switch]$All
+     )
 
-Get-ADUser -Filter "Name -like '*$Name*'" -Properties * | Select-Object Name, SamAccountName, mail
+if($all){
+Get-ADUser -Filter {(Name -like '$Name*')} -Properties * | Select-Object Name, SamAccountName, Mail
+}
+
+else {
+Get-ADUser -Properties * -Filter "Name -like '$Name*'" | Where {$_.Enabled -contains "True"}  | Select-Object Name, SamAccountName, Mail
+
+}
+
