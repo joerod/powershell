d4e6ed14010c2f65c1d8a6f584ad214cfd39f278
Joe Rodriguez <joerod@gmail.com>
2014-02-19 10:22:44 -0500

    Update remove_null_groups.ps1

--- remove_null_groups.ps1
+++ remove_null_groups.ps1
@@ -1,9 +1,9 @@
  #This script finds empty groups in the OU listed in the 4th line
 
  function empty_groups{
- Get-ADGroup -Filter {GroupCategory -eq 'Security'} -SearchBase "OU=Empty,DC=contoso, DC=local" `
- | ?{@(Get-ADGroupMember $_).Length -eq 0} `
- | Select-Object Distinguishedname -expandproperty Distinguishedname 
+ Get-ADGroup -Filter {GroupCategory -eq 'Security'} -SearchBase "OU=Empty,DC=contoso, DC=local" | 
+ {@(Get-ADGroupMember $_).Length -eq 0} | 
+ Select-Object Distinguishedname -expandproperty Distinguishedname 
  }
 
 foreach($i in empty_groups){
