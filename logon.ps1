f704bb77ea0729a7a3937ca148ae02376f7691b8
Joe Rodriguez <joerod@gmail.com>
2014-02-19 10:25:34 -0500

    Update logon.ps1

--- logon.ps1
+++ logon.ps1
@@ -1,1 +1,2 @@
-(Get-date -UFormat "%a %m/%d/%Y,%H:%M:%S"),$env:COMPUTERNAME,$env:USERNAME,$env:SESSIONNAME,$env:logonserver -join "," | Out-File "\\Path\to\Share\Logon.log"
+(Get-date -UFormat "%a %m/%d/%Y,%H:%M:%S"),$env:COMPUTERNAME,$env:USERNAME,$env:SESSIONNAME,$env:logonserver -join "," |
+  Out-File "\\Path\to\Share\Logon.log"
