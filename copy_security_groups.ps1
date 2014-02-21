6a7c1c72f9676d2d0deb49315602a692afb87576
Joe Rodriguez <joerod@gmail.com>
2014-02-21 09:24:43 -0500

    Update copy_security_groups.ps1

--- copy_security_groups.ps1
+++ copy_security_groups.ps1
@@ -1,7 +1,6 @@
 $Source_Group = "CN=GouptoCopy,OU=Groups,OU=Resources,DC=contoso,DC=LOCAL" 
 $Destination_Group = "CN=CopytoGroup,OU=Groups,OU=Resources,DC=contoso,DC=LOCAL" 
- 
-$Target = Get-ADGroupMember -Identity $Source_Group 
-foreach ($Person in $Target) { 
+
+foreach ($Person in ($Target)Get-ADGroupMember -Identity $Source_Group)) { 
     Add-ADGroupMember -Identity $Destination_Group -Members $Person.distinguishedname 
 }
