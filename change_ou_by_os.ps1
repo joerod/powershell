48e7a0649b004d725006185d5b16d211341f461e
Joe Rodriguez <joerod@gmail.com>
2014-03-17 15:03:18 -0400

    Create change_ou_by_os.ps1

---
+++ change_ou_by_os.ps1
@@ -0,0 +1,10 @@
+<#this script will move a machine to the proper OU based on OS
+
+#>
+$machines = Get-ADObject -SearchBase "OU=WIN7,DC=CONTOSO,DC=LOCAL" -Filter * -Properties * |
+  select operatingSystem, DistinguishedName
+foreach ($machine in $machines){
+  if($machine.operatingSystem  -match "Windows XP Professional"){
+   Move-ADObject -Identity $machine.DistinguishedName -TargetPath "OU=WINXP,DC=CONTOSO,DC=LOCAL"
+   }
+  }
