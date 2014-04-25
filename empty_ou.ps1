5d34af3833a5d09acf8a7a293cbae21b5c840d26
Joe Rodriguez <joerod@gmail.com>
2014-04-25 00:00:43 -0400

    Update empty_ou.ps1

--- empty_ou.ps1
+++ empty_ou.ps1
@@ -1,9 +1,9 @@
 #This script finds empty sub OUs in the OU listed in the 4th line and removes them
 
 function Get-EmptyOU{
-Get-ADOrganizationalUnit -Filter * -SearchBase "OU=Empty,DC=contoso, DC=local" `
-| Where-Object {-not ( Get-ADObject -Filter * -SearchBase $_.Distinguishedname -SearchScope OneLevel -ResultSetSize 1 )}`
-| Select-Object -ExpandProperty Distinguishedname
+Get-ADOrganizationalUnit -Filter * -SearchBase "OU=Empty,DC=contoso, DC=local" |
+  Where-Object {-not ( Get-ADObject -Filter * -SearchBase $_.Distinguishedname -SearchScope OneLevel -ResultSetSize 1 )} | 
+  Select-Object -ExpandProperty Distinguishedname
 }
 
 #this removes each empty OU
