2841ccb3ad191d325ce4c8166e555e94f96fc45d
Joe Rodriguez <joerod@gmail.com>
2014-05-16 09:01:26 -0400

    Update remove_local_admin_domain_users.ps1

--- remove_local_admin_domain_users.ps1
+++ remove_local_admin_domain_users.ps1
@@ -2,7 +2,7 @@
 
 Function Get-AdminGroups{
 
-foreach($i in (Get-Content C:\Users\admin\Desktop\users.txt)){
+foreach($i in (Get-Content C:\Users\joerod\Desktop\remove_users.txt)){
 
 #test if machine is on the network
 if (-not (Test-Connection -computername $i -count 1 -Quiet -ErrorAction SilentlyContinue)) {
@@ -37,45 +37,52 @@ Function Remove-Admin{
 
   #deletes local accounts if conditions are met
   for($i=0;$i -lt $admin.users.count;$i++){
-  if(($admin.users[$i] -ne "Contoso\Domain Admins") -and ($admin.users[$i] -notlike "Contoso\WkstPerm*") -and ($admin.users[$i] -notlike "Administrator") -and ($admin.users[$i] -ne $null)){
+  if(($admin.users[$i] -ne "contoso\Domain Admins") -and ($admin.users[$i] -notlike "contoso\WkstPerm*") -and ($admin.users[$i] -notlike "administrator") -and ($admin.users[$i] -ne $null)){
     
     #Add user to Remote Desktop Users Group
-    
    Try{
    # adds logged in use to remote desktop users group
     $logoff_user = gwmi -computername $($admin.computername) -class win32_computersystem |select -ExpandProperty UserName
+      if($logoff_user -ne $null){
     $scriptblock = $ExecutionContext.InvokeCommand.NewScriptBlock("NET LOCALGROUP 'Remote Desktop Users' $logoff_user /add")
-    Invoke-Command -ComputerName $($admin.computername) -ScriptBlock $scriptblock -ErrorVariable remotedesktop
+    Invoke-Command -ComputerName $($admin.computername) -ScriptBlock $scriptblock -ErrorAction Stop -ErrorVariable remotedesktop
     }
+     }
      Catch{
-    Write-Warning "$($admin.users[$i]) is already a member of the  remote users group." | Out-File C:\Script_logs\remove_local_admin$(get-date -f MM-dd-yyyy).log -append
+    Write-Warning "$($admin.users[$i]) is already a member of the remote users group." #| Out-File C:\Script_logs\remove_local_admin$(get-date -f MM-dd-yyyy).log -append
     
     }
-
+    
     Try{
     # add users who are in admin group to remote admin group
     $scriptblock2 = $ExecutionContext.InvokeCommand.NewScriptBlock("NET LOCALGROUP 'Remote Desktop Users' $($admin.users[$i]) /add")
-    Invoke-Command -ComputerName $($admin.computername) -ScriptBlock $scriptblock2 -ErrorVariable remotedesktop
-    Write-Output "Add $($admin.users[$i]) to Remote Desktop Users group on $($admin.computername)`r" |Out-File C:\Script_logs\remove_local_admin.log -Append
+    Invoke-Command -ComputerName $($admin.computername) -ScriptBlock $scriptblock2 -ErrorAction Stop -ErrorVariable remotedesktop
+    Write-Output "Add $($admin.users[$i]) to Remote Desktop Users group on $($admin.computername)`r" #|Out-File C:\Script_logs\remove_local_admin.log -Append
     $remotedesktop  | Out-File C:\Script_logs\remove_local_admin_$(get-date -f MM-dd-yyyy).log -append
   }
    Catch{
-    Write-Warning "$($admin.users[$i]) is already a member of the  remote users group." | Out-File C:\Script_logs\remove_local_admin_$(get-date -f MM-dd-yyyy).log -append
+    Write-Warning "$($admin.users[$i]) is already a member of the  remote users group." #| Out-File C:\Script_logs\remove_local_admin_$(get-date -f MM-dd-yyyy).log -append
     $($admin.users[$i]) | Out-File C:\Script_logs\remove_local_admin_$(get-date -f MM-dd-yyyy).log -append
     }
    
     #Removes user from Administrators Group
     $scriptblock = $ExecutionContext.InvokeCommand.NewScriptBlock("NET LOCALGROUP administrators $($admin.users[$i]) /delete")
-    Invoke-Command -ComputerName $($admin.computername) -ScriptBlock $scriptblock -ErrorVariable $admingroup
-    Write-Output "Delete $($admin.users[$i]) from Administrators group on $($admin.computername)`r" |Out-File C:\Script_logs\remove_local_admin_$(get-date -f MM-dd-yyyy).log -Append
+    Invoke-Command -ComputerName $($admin.computername) -ScriptBlock $scriptblock -ErrorAction Stop -ErrorVariable $admingroup
+    Write-Output "Delete $($admin.users[$i]) from Administrators group on $($admin.computername)`r" #|Out-File C:\Script_logs\remove_local_admin_$(get-date -f MM-dd-yyyy).log -Append
 
-    #Logs off user
 
-    Invoke-Command -ComputerName $($admin.computername) { (gwmi win32_operatingsystem).Win32Shutdown(4) |Out-Null} -ErrorVariable logoff
-    Write-Output "Logging off $logoff_user" |Out-File C:\Script_logs\remove_local_admin_$(get-date -f MM-dd-yyyy).log -Append
+    
    }
   }
- }
+ #Logs off user
+    Try{
+    Invoke-Command -ComputerName $($admin.computername) { (gwmi win32_operatingsystem).Win32Shutdown(4) |Out-Null} -ErrorAction Stop -ErrorVariable logoff
+    Write-Output "Logging off $logoff_user" #|Out-File C:\Script_logs\remove_local_admin_$(get-date -f MM-dd-yyyy).log -Append
+   }
+    Catch{
+   Write-Output "Cannot log off $logoff_user" #|Out-File C:\Script_logs\remove_local_admin_$(get-date -f MM-dd-yyyy).log -Append
+    }
+   }
 }
  
  
