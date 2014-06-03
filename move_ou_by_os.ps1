2a7ba1c7f60adfa09cb65d9049e0e7b12ff37905
Joe Rodriguez <joerod@gmail.com>
2014-06-03 10:25:25 -0400

    Update move_ou_by_os.ps1

--- move_ou_by_os.ps1
+++ move_ou_by_os.ps1
@@ -6,5 +6,6 @@ $machines = Get-ADObject -SearchBase "OU=WIN7,DC=CONTOSO,DC=LOCAL" -Filter * -Pr
 foreach ($machine in $machines){
   if($machine.operatingSystem  -match "Windows XP Professional"){
    Move-ADObject -Identity $machine.DistinguishedName -TargetPath "OU=WINXP,DC=CONTOSO,DC=LOCAL"
+   Write-Output "$machine has been moved"
    }
   }
