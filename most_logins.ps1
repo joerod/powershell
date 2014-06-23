eab6ed41dbc00e36430ac9901bc4fa37cfb776c0
Joe Rodriguez <joerod@gmail.com>
2014-06-23 17:11:45 -0400

    Update most_logins.ps1

--- most_logins.ps1
+++ most_logins.ps1
@@ -1,25 +1,84 @@
 <# This script uses my user login script (https://github.com/joerod/powershell/blob/master/logon.ps1) log and finds 
 what machine the user logs into most.  In my environment users log into several machines, this makes finding their primary
 machine much easier.  I will use this script in subsequent scripts to auto find the users primary machine and preform an 
-action
+action.
+
+I've added the ablity to choose either a file that contains a list of users to find most logins or one user
+I've also added a fix for users with apostrophe's in their name
 #>
 
-Param(
-[Parameter(Position=0,mandatory=$true)]
-[string]$Name
-)
+[CmdletBinding(DefaultParameterSetName="Name")]
+param(
+    [Parameter(Position=0, ParameterSetName="File")]
+    [string]
+    $File,
 
-foreach($user in (Get-ADUser -Properties * -Filter "Name -like '$Name*'" | ? {$_.Enabled -contains "True"})){
-$array = Import-Csv \\Path\to\Share\Logon.log | ? {$_.username -eq $user.samaccountname} | % {
+    [Parameter(Position=0, ParameterSetName="Name")]
+    [string]
+    $Name
+    )
 
-New-Object -TypeName PSCustomObject -Property @{
-    Name = $user.name
-    SAMAccoutName = $user.samaccountname
-    ComputerName = $_.computername 
-     } 
-    }
+if ($psCmdlet.ParameterSetName -eq "File") {
+foreach($name in gc $file){
+  #if name has an apostrophe
+  if($name.Contains("'")){
+      $name = ($name.replace("'","''"))
+   }
+
+try{
+    foreach($user in (Get-ADUser -Properties * -Filter "Name -like '$Name*'" -ErrorAction Stop | ? {$_.Enabled -contains "True"})){
+    $array = Import-Csv \\Path\to\Share\Logon.log | ? {$_.username -eq $user.samaccountname} | % {
+
+    New-Object -TypeName PSCustomObject -Property @{
+        Name = $user.name
+        SAMAccoutName = $user.samaccountname
+        ComputerName = $_.computername 
+      
+         } 
+        }
 
 $array | sort | group ComputerName | sort Count | select -ExpandProperty Group | select -last 1
-    
 
+    
+    }
 }
+
+
+catch{
+Write-Warning "Cannot find $name"
+
+  }
+ }
+}
+
+else {
+
+  #if name has an apostrophe
+  if($name.Contains("'")){
+      $name = ($name.replace("'","''"))
+   }
+
+try{
+    foreach($user in (Get-ADUser -Properties * -Filter "Name -like '$Name*'" -ErrorAction Stop | ? {$_.Enabled -contains "True"})){
+    $array = Import-Csv \\Path\to\Share\Logon.log | ? {$_.username -eq $user.samaccountname} | % {
+
+    New-Object -TypeName PSCustomObject -Property @{
+        Name = $user.name
+        SAMAccoutName = $user.samaccountname
+        ComputerName = $_.computername 
+      
+         } 
+        }
+
+$array | sort | group ComputerName | sort Count | select -ExpandProperty Group | select -last 1
+
+    
+    }
+}
+
+
+catch{
+Write-Warning "Cannot find $name"
+
+  }
+ }
