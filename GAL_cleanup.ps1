834f5fd1a9325793df1d178e8f38767dcc4575b4
Joe Rodriguez <joerod@gmail.com>
2014-06-24 09:38:50 -0400

    Create GAL_cleanup.ps1

---
+++ GAL_cleanup.ps1
@@ -0,0 +1,26 @@
+<#
+    .SYNOPSIS 
+        This script looks for accounts in AD that ane disabled, then checkes Exchange to see if the account is still visable.
+        If you'd like to remove the user from the GAL uncomment line 15
+     
+#>
+
+$disabled = Get-ADUser -Properties * -Filter {Enabled -eq $false}
+  foreach($user in $disabled){
+  try{
+  if((get-Mailbox $($user.SamAccountName) -ErrorAction Stop | select -expand HiddenFromAddressListsEnabled) -eq $false){
+
+
+  Write-Output $user.DisplayName
+  
+  
+  #Set-Mailbox $user.SamAccountName -HiddenFromAddressListsEnabled $true
+    }
+  }
+
+ catch {
+ Write-Warning "There is no mailbox for $($user.DisplayName)"
+
+ }
+
+}
