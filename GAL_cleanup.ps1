1f00fa545ff42ba96b66dd6747387fc16deb004a
Joe Rodriguez <joerod@gmail.com>
2014-06-24 09:40:18 -0400

    Update GAL_cleanup.ps1

--- GAL_cleanup.ps1
+++ GAL_cleanup.ps1
@@ -1,7 +1,7 @@
 <#
     .SYNOPSIS 
         This script looks for accounts in AD that are disabled, then checkes Exchange to see if the account is still visable
-        in the GAL.  If you'd like to remove the user from the GAL uncomment line 15
+        in the GAL.  If you'd like to remove the user from the GAL uncomment line 17
      
 #>
 
