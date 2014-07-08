cb9d34cdd1b88c4993bcf357ff506d9252cda3cf
Joe Rodriguez <joerod@gmail.com>
2014-07-08 13:49:43 -0400

    Update remove_dns.ps1

--- remove_dns.ps1
+++ remove_dns.ps1
@@ -1,6 +1,32 @@
-#This script reads off a csv with column 1 being a list of IPs and column 2 the corresponding computer names.  
-ForEach($ip in (Import-Csv C:\temp\removedns.csv){
+<#
+    .SYNOPSIS 
+        This script will query all the zones on a DNS server then remove entries based on a criteria in this case IP
+        Addresses
+    .EXAMPLE
+     remove_dns.ps1.ps1
+ 
+#>
 
-dnscmd dnsserver /RecordDelete contoso.local $ip.name A $ip.ip /f
-Write-Output "$($ip.name) has been removed from DNS" 
+
+$domainnames =  Get-DnsServerZone -ComputerName dnsserver | ? {($_.IsReverseLookupzone -like "false") -and ($_.ZoneType -like "Primary") } |
+  select -ExpandProperty ZoneName
+
+foreach ($domainname in $domainnames){
+function list{
+
+get-wmiobject -ComputerName crpnycdcrt01 -Namespace root\microsoftDNS -Class MicrosoftDNS_ResourceRecord -Filter "domainname='$domainname'" |
+ select IPAddress, ownername |
+  ? {($_.IPAddress -like '172.17.*') -or ($_.IPAddress -like '172.19.*') -and ($_.IPAddress -notlike $null)} |
+  Sort IPAddress #|
+  #export-csv C:\Users\jorodriguez\Desktop\$domainname.csv  -NoTypeInformation
+  }
+
+
+ForEach($ip in list){
+#gives a usable DNS name  
+$ipnew = $ip.ownername -replace "\..+"
+#Remove-DnsServerResourceRecord -ZoneName "corporate.local" -RRType "A" -Name $($ip.ownername) -RecordData $($ip.IPAddress)
+dnscmd crpnycdcrt01 /RecordDelete $domainname $ipnew A $ip.IPAddress /f
+Write-output "$ipnew $($ip.IPAddress) has been removed from DNS" 
 }
+ }
