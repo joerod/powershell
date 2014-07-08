a4a530a188d74c2d2cfa482a86620207c0629066
Joe Rodriguez <joerod@gmail.com>
2014-07-08 10:06:45 -0400

    Update delete_user_folders.ps1

--- delete_user_folders.ps1
+++ delete_user_folders.ps1
@@ -1,6 +1,6 @@
 <#
     .SYNOPSIS 
-        This script will find user folders and delete them.  I've added some logic to omit folders that should not be deleted.
+        This script will find user folders in C:\Users and delete them.  I've added some logic to omit folders that should not be deleted.
         WinRM is used to connect to each box and run a cmd command
         
     .EXAMPLE
@@ -16,7 +16,7 @@ foreach ($computer in $computers){
      Invoke-Command -ComputerName $computer {
     $folders = Get-ChildItem C:\Users |select -expand name
       foreach ($folder in $folders){
-      if(($folder -notlike "Public") -and ($folder -notlike "administrator") -and ($folder -notlike "default")){
+      if(($folder -notlike "Public") -and ($folder -notlike "administrator") -and ($folder -notlike "Default")){
          Write-Output "removing $folder"
          cmd.exe /c del  "c:\Users\$folder" /f/s/q/a
          cmd.exe /c rmdir  "c:\Users\$folder" /s/q}
