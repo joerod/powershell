ce02d8336cf5c8607b1a29e2ad3ecc84d6e249e1
Joe Rodriguez <joerod@gmail.com>
2014-07-29 15:17:07 -0400

    Update GAL_cleanup.ps1

--- GAL_cleanup.ps1
+++ GAL_cleanup.ps1
@@ -1,25 +1,54 @@
 <#
     .SYNOPSIS 
         This script looks for accounts in AD that are disabled, then checkes Exchange to see if the account is still visable
-        in the GAL.  If you'd like to remove the user from the GAL uncomment line 16
+        in the GAL.  
+
+     .SYNTAX
+        Gal_cleanup.ps1 [-Remove] 
+
+     .EXAMPLE
+         -------------------------- EXAMPLE 1 --------------------------
+        PS C:\>create_admin_account.ps1 
+
+        This will show all accounts that are disabled in AD but still visable in the GAL.
+
+         -------------------------- EXAMPLE 2 --------------------------
+        PS C:\>create_admin_account.ps1 -remove
+
+        This will remove all accounts that are disabled in AD but still visable in the GAL.
+
+  
      
 #>
+[CmdletBinding()]
+param([switch] $Remove)
+
+function GAL_Cleanup{
 
   foreach($user in (Get-ADUser -Properties * -Filter {Enabled -eq $false})){
   try{
   if((get-Mailbox $($user.SamAccountName) -ErrorAction Stop | select -expand HiddenFromAddressListsEnabled) -eq $false){
 
-
-  Write-Output $user.DisplayName
   
+New-Object -TypeName PSCustomObject -Property @{
+    SAMAccoutName = $user.samaccountname
+    Name = $user.name
+   
+     } 
   
-  #Set-Mailbox $user.SamAccountName -HiddenFromAddressListsEnabled $true
+  if($remove){
+  Set-Mailbox $user.SamAccountName -HiddenFromAddressListsEnabled $true
+  Write-Output "$($user.SamAccountName) has been removed from the GAL"
+  }
     }
   }
 
  catch {
- Write-Warning "There is no mailbox for $($user.DisplayName)"
+ Write-Verbose "There is no mailbox for $($user.DisplayName)" 
 
  }
 
+ }
 }
+
+GAL_Cleanup
