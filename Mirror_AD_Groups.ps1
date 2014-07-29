d27e25488425e22bf1279eb9b906a889069db33f
Joe Rodriguez <joerod@gmail.com>
2014-07-29 14:18:28 -0400

    Update Mirror_AD_Groups.ps1

--- Mirror_AD_Groups.ps1
+++ Mirror_AD_Groups.ps1
@@ -1,11 +1,16 @@
+<#
+    .SYNOPSIS 
+        Copies AD security groups from one user to another
+     
+#>
+
 #Added a Param to make it easier to run this script use -CopyUser and -ToUser
 Param(
 [String]$CopyUser,
 [String]$ToUser
 )
 Function Copy-AdGroups{
-$groupname = get-aduser $CopyUser -Properties memberOF |Select-Object memberOF -ExpandProperty memberOF
-foreach($i in $groupname){
+foreach($i in (get-aduser $CopyUser -Properties memberOF |Select-Object memberOF -ExpandProperty memberOF)){
 Add-ADPrincipalGroupMembership -Identity $ToUser -MemberOf $i
 Write-Host "Copied $i to $ToUser"
 }
