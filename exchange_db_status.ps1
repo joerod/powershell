63050506edeaf94422fa6e1c9454cdc4b31a02df
Joe Rodriguez <joerod@gmail.com>
2014-07-29 23:17:52 -0400

    Update exchange_db_status.ps1

--- exchange_db_status.ps1
+++ exchange_db_status.ps1
@@ -10,16 +10,15 @@ $style = $style + "</style>"
 
 Function Get-DBstatus{
 foreach($server in (@("mailboxserver1","mailboxserver2","mailboxserver3"))){
-Get-MailboxDatabaseCopyStatus -Server $server |Select DatabaseName, Mailboxserver,Status |? {($_.Status -ne "Healthy") -and ($_.Status -ne "Mounted") -and ($_.Status -notlike "Seeding*") } |ft
+Get-MailboxDatabaseCopyStatus -Server $server |Select DatabaseName, Mailboxserver,Status |? {($_.Status -ne "Healthy") -and ($_.Status -ne "Mounted") -and ($_.Status -notlike "Seeding*") } 
    # ConvertTo-Html -Head $style
 
 
     }
   }
-  $status = Get-DBstatus
-
+  
   if($status -ne 0){
   Send-MailMessage -To  joerod@contoso.com `
--from exchangedbalerts@contoso.com -Body ($status|Out-String) -subject "Error on Exchange DB" -smtpserver stmpmailbox -priority High
+-from exchangedbalerts@contoso.com -Body (Get-DBstatus|Out-String) -subject "Error on Exchange DB" -smtpserver stmpmailbox -priority High
   }
 
