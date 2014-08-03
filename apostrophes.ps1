43197f8809ab4d229326c50148e161c225340898
Joe Rodriguez <joerod@gmail.com>
2014-08-02 22:55:36 -0400

    Update apostrophes.ps1

--- apostrophes.ps1
+++ apostrophes.ps1
@@ -1,7 +1,7 @@
 #i'll use this script in future scripts when using the get-aduser cmdlet.  I had problems when a user has an apostrophes
 #in their name
 
-a = "Jermaine O'Neal"
+$a = "Jermaine O'Neal"
 if($a.contains("'")){
 
 $a = ($a.replace( "'","''"))
