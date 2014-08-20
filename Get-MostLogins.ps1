1e12022148cd4a3040bc19c656fa1e246495c794
Joe Rodriguez <joerod@gmail.com>
2014-08-20 11:26:18 -0400

    Update and rename most_logins.ps1 to Get-MostLogins.ps1

--- Get-MostLogins.ps1
+++ Get-MostLogins.ps1
@@ -1,27 +1,40 @@
+function Get-MostLogins {
 <#
     .SYNOPSIS 
+        Finds all domain machines a user has logged into by using the users 'real' name
+    
+    .DESCRIPTION
         This script uses my user login script log and finds 
         what machine the user logs into most.  In my environment users log into several machines, this makes finding their primary
         machine much easier.  I will use this script in subsequent scripts to auto find the users primary machine and preform an 
         action.
 
         I've added the ablity to choose either a file that contains a list of users to find most logins or one user
-        I've also added a fix for users with apostrophes in their name
+        I've also added a fix for users with apostrophe's in their name
 
-    .EXAMPLE
-     most_logins.ps1 -Name "Joe Rodriguez"
-     This command will show what machine the single user Joe Rodriguez logged into the most
-
-    .EXAMPLE
-     most_logins.ps1 -File c:\users\joerod\desktop\users.txt
-     This command will show what machine all the users in the text file users.txt logged into the most.  Add one name per
-     line of text eg.
+    .PARAMETER Name
+        Searches a single user by their 'real' name
      
-     john doe
-     jane doe
-     steve smith
+    .PARAMETER File
+        Takes a text file with a list of users 'real' names and queries them with this script.  You must specify full path to file
+    
+    .PARAMETER Full
+        Lists all the machines a user has logged into
+    
+    .EXAMPLE
+        Get-MostLogins -Name "Joe Rodriguez"
+        This command will show what machine Joe Rodriguez logged into the most
+
+    .EXAMPLE
+         Get-MostLogins -File c:\users\joerod\desktop\users.txt
+         This command will show what machine all the users in the text file users.txt logged into the most
+    
+    .EXAMPLE
+        Get-MostLogins -Name "Joe Rodriguez" -Full
+        This command will show all machines Joe Rodriguez logged into
 
   #>
+
 
 [CmdletBinding(DefaultParameterSetName="Name")]
 param(
@@ -31,7 +44,9 @@ param(
 
     [Parameter(Position=0, ParameterSetName="Name")]
     [string]
-    $Name
+    $Name,
+
+    [switch]$Full
     )
 
 if ($psCmdlet.ParameterSetName -eq "File") {
@@ -42,21 +57,25 @@ foreach($name in gc $file){
    }
 
 try{
+   
     foreach($user in (Get-ADUser -Properties * -Filter "Name -like '$Name*'" -ErrorAction Stop | ? {$_.Enabled -contains "True"})){
     $array = Import-Csv \\Path\to\Share\Logon.log | ? {$_.username -eq $user.samaccountname} | % {
 
     New-Object -TypeName PSCustomObject -Property @{
         Name = $user.name
         SAMAccoutName = $user.samaccountname
-        ComputerName = $_.computername 
-      
+        ComputerName = $_.computername     
          } 
         }
+             if($full){
+             $array
+             }
 
-$array | sort | group ComputerName | sort Count | select -ExpandProperty Group | select -last 1
-
-    }
-}
+             else{
+            $array | sort | group ComputerName | sort Count | select -ExpandProperty Group | select -last 1
+                 }
+               }
+            }
 
 catch{
 Write-Warning "Cannot find $name"
@@ -66,13 +85,15 @@ Write-Warning "Cannot find $name"
 }
 
 else {
+
   #if name has an apostrophe
   if($name.Contains("'")){
       $name = ($name.replace("'","''"))
    }
 
 try{
-    foreach($user in (Get-ADUser -Properties * -Filter "Name -like '$Name*'" -ErrorAction Stop | ? {$_.Enabled -contains "True"})){
+  
+    foreach($user in (Get-ADUser -Properties * -Filter "Name -like '$Name*'"  | ? {$_.Enabled -contains "True"})){
     $array = Import-Csv \\Path\to\Share\Logon.log | ? {$_.username -eq $user.samaccountname} | % {
 
     New-Object -TypeName PSCustomObject -Property @{
@@ -81,11 +102,18 @@ try{
         ComputerName = $_.computername 
          } 
         }
+             if($full){
+             $array
+             }
 
-$array | sort | group ComputerName | sort Count | select -ExpandProperty Group | select -last 1
-  }
-}
+             else{
+            $array | sort | group ComputerName | sort Count | select -ExpandProperty Group | select -last 1
+                  }
+              }
+            }
 catch{
 Write-Warning "Cannot find $name"
-   }
+
+    }
+  }
  }
