4b135cc8705ebf7351ad1cc65f8dbd28c9460af4
Joe Rodriguez <joerod@gmail.com>
2014-11-10 10:38:38 -0500

    Create remove_manager.ps1
    this script checks for disabled AD users and removes their manager.

---
+++ remove_manager.ps1
@@ -0,0 +1,7 @@
+foreach($user in (get-aduser -filter *)){
+$account = get-aduser $user -Properties enabled, manager
+if($account.enabled -eq $false -and $account.manager -ne $null){
+Write-Output "Working on $($account.name)"
+set-aduser $account -Manager $null
+}
+ }
