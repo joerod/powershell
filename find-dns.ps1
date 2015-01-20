fe859c3c78e8fe9fd87b46fa0f485e7a804ee92c
Joe Rodriguez <joerod@gmail.com>
2015-01-20 15:42:07 -0500

    Update find-dns.ps1

--- find-dns.ps1
+++ find-dns.ps1
@@ -1,6 +1,6 @@
 <#
     .SYNOPSIS 
-      Searches DNS for records based on user input.  The -DeleteRecord switch will remove the record.  User can also specify DNS and zone to query.
+      Searches DNS for records based on user input.  The -DeleteRecord switch will remove the record.
     .EXAMPLE
      Find-DNS -Search Test
      This command finds records in DNS named "Test"
@@ -14,11 +14,11 @@ function Find-DNS {
 param(
     [Parameter(Position=1)]
     [string]
-    $Zone = "contoso.local",
+    $Zone = "Contoso.local",
 
     [Parameter(Position=2)]
     [string]
-    $DNS = "DNSServer",
+    $Server = "DNS_Server",
 
     [Parameter(Position=0, Mandatory=$True)]
     [string]
@@ -27,8 +27,15 @@ param(
     [switch]$DeleteRecord
     )
 
+# Script requires Powershell 4 or greater.
+$version = (Get-Host).Version.major
+if($version -lt 4){
+ Write-Output "You need to upgrade your powershell version in order to run this script."
+ Write-Output "Your current verion is $version you need verion 4 or higher"  
+ exit
+}
 
-$arecord = Get-DnsServerResourceRecord -ComputerName $DNS -ZoneName $Zone | ? {$_.HostName -like "*$search*"}
+$arecord = Get-DnsServerResourceRecord -ComputerName $Server -ZoneName $Zone | ? {$_.HostName -like "*$search*"}
 $arecord
  if($arecord -eq $null){
    Write-Output "Record for $search cannot be found"
@@ -41,7 +48,7 @@ $arecord
 if($DeleteRecord -and $status -eq 1){
 
 $arecord[1] | %{
-  Remove-DnsServerResourceRecord -ComputerName $DNS -ZoneName $Zone -RRType $_.RecordType -Name $_.hostname -RecordData $_.RecordData.ipv4address.IPAddressToString -Force
+  Remove-DnsServerResourceRecord -ComputerName $Server -ZoneName $Zone -RRType $_.RecordType -Name $_.hostname -RecordData $_.RecordData.ipv4address.IPAddressToString -Force
    } 
    Write-Host "$arecord[1] has been removed" -backgroundcolor Red
   }
