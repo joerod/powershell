9af5b94e3057f34c0f517eb582f3fb2ec0779f50
Joe Rodriguez <joerod@gmail.com>
2016-06-13 09:06:07 -0400

    Create LocalGroupRights.psm1
    Since there are no out of the box PS cmdlets to check remote local groups I created this module.

---
+++ LocalGroupRights.psm1
@@ -0,0 +1,44 @@
+#Joe Rodriguez
+#joerod@gmail.com
+#version 1.0
+
+Function Get-LocalGroupRights {
+ Param (
+        [Parameter(Mandatory = $true,Position=0)][string] $ComputerName,
+        [Parameter(Position=1)][string] $Group = 'Administrators'
+        
+    )
+$GroupInfo = [ADSI] "WinNT://$ComputerName/$Group,group"
+$Members = $GroupInfo.psbase.Invoke("Members")
+$Members | %{ $_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null) }
+}
+
+#Get-LocalGroupRights -ComputerName Computer001
+
+Function Add-LocalGroupRights {
+ Param (
+        [Parameter(Mandatory = $true,Position=0,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)][string] $ComputerName,
+        [Parameter(Mandatory = $true,Position=1,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)][string] $Name,
+        [Parameter(Mandatory = $true, Position = 2)][ValidateSet("User", "Group")] $Type,
+        [Parameter(Position = 3)] $Group = 'Administrators'
+    )
+$GroupInfo = [ADSI] "WinNT://$ComputerName/$Group,group"
+$ToAdd = [ADSI] "WinNT://AD.MLP.COM/$Name,$Type"
+$GroupInfo.Add($ToAdd.Path)
+}
+
+#Add-LocalGroupRights -ComputerName Computer001 -Name joerod -Type User
+
+Function Remove-LocalGroupRights {
+ Param (
+        [Parameter(Mandatory = $true,Position=0,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)][string] $ComputerName,
+        [Parameter(Mandatory = $true,Position=1,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)][string] $Name,
+        [Parameter(Mandatory = $true, Position = 2)][ValidateSet("User", "Group")] $Type,
+        [Parameter(Position = 3)] $Group = 'Administrators'
+    )
+$GroupInfo = [ADSI] "WinNT://$ComputerName/$Group,group"
+$ToRemove = [ADSI] "WinNT://AD.MLP.COM/$Name,$Type"
+$GroupInfo.Remove($ToRemove.Path)
+}
+
+#Remove-LocalGroupRights -ComputerName Computer001 -Name joerod -Type User
