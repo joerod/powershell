5d96512e83a3f6bb40d00fe7b538aaa61fd3e98b
Joe Rodriguez <joerod@gmail.com>
2016-09-18 11:51:25 -0400

    Update Mirror_AD_Groups.ps1

--- Mirror_AD_Groups.ps1
+++ Mirror_AD_Groups.ps1
@@ -5,12 +5,15 @@
 #>
 
 #Added a Param to make it easier to run this script use -CopyUser and -ToUser
+
+Function Copy-AdGroups{
+    
 Param(
 [String]$CopyUser,
 [String]$ToUser
 )
-Function Copy-AdGroups{
-foreach($GroupMember in ((get-aduser $CopyUser -Properties memberOF).memberOF){
+    
+Foraach($GroupMember in ((get-aduser $CopyUser -Properties memberOF).memberOF){
 Add-ADPrincipalGroupMembership -Identity $ToUser -MemberOf $GroupMember
 Write-Verbose ("Copied {0} to {1}" -f $GroupMember,$ToUser)
 }
