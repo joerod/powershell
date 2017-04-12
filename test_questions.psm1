8a034cb65d8ff532a18644d6a294ef84de0c9952
Joe Rodriguez <joerod@gmail.com>
2017-04-12 01:04:43 -0400

    Update test_questions.psm1

--- test_questions.psm1
+++ test_questions.psm1
@@ -1,11 +1,12 @@
 #Was thinking about PowerShell questions to ask on an interview and I came up with these. 
 
+#How do i know if a number is negative or positive
 Function Get-NegativeOrPositive([int]$Number){
  if($Number -gt 0){Write-Output "Postive"} else{Write-Output "Negative"}
  }
 
  #Get-NegativeOrPositive -Number '-1'
-
+#How do i know if a number is even or odd
  Function Get-EvenOrOdd([int]$Number){
  if($Number%2 -eq 0){Write-Output "Even"} else{Write-Output "Odd"}
  }
