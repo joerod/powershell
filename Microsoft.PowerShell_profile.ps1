bba792c6e6e10118f3d169efdf96bc2cf2a7e02f
Joe Rodriguez <joerod@gmail.com>
2018-09-12 22:52:46 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,7 +8,7 @@ Install-Module -Name posh-git -Scope CurrentUser
  function prompt {
     $userinfo = (($env:USERNAME).tolower() + "@" + ($env:COMPUTERNAME).tolower())
     $date = Get-Date -Format u
-    $prompt = "PS "
+    $prompt = Write-Prompt ("PS {0}.{1}" -f $PSVersionTable.PSVersion.Major,$PSVersionTable.PSVersion.Minor)
     $prompt += Write-Prompt " $userinfo | " -ForegroundColor Green
     $prompt += & $GitPromptScriptBlock 
     $prompt += Write-Prompt " | $date "-ForegroundColor Magenta
