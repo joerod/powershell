e363f11948df12e9e79c8944d29784e403638403
Joe Rodriguez <joerod@gmail.com>
2019-04-27 00:07:40 -0400

    Update Remove-ItemByDate.ps1

--- Remove-ItemByDate.ps1
+++ Remove-ItemByDate.ps1
@@ -8,14 +8,14 @@ Adds a file name extension to a supplied name.
 Takes any strings for the file name or extension.
 
 .PARAMETER Item
-The file or folder to be compared to 
+The file or folder to be compared to
 
 .PARAMETER Date
 The date the the file or folder must be younger than
 
 .EXAMPLE
 
-Remove-ItemByDate -Item D:\JoeRod\joerod\Desktop\movies -Date (Get-Date).AddMonths(-2) 
+Remove-ItemByDate -Item D:\JoeRod\joerod\Desktop\movies -Date (Get-Date).AddMonths(-2)
 #>
   [CmdletBinding(SupportsShouldProcess=$True)]
   param(
@@ -23,10 +23,22 @@ Remove-ItemByDate -Item D:\JoeRod\joerod\Desktop\movies -Date (Get-Date).AddMont
   [datetime]$Date
   )
 
-  foreach($item_object in (Get-ChildItem $Item -Recurse)){
+  $args = @{
+    Recurse = $True
+  }
+
+  if((Get-item $item).psiscontainer) {
+    $args.Recurse = $true
+  }
+  else {
+    $args.Recurse = $false
+  }
+
+
+  foreach($item_object in (Get-ChildItem $Item @args)){
     if($item_object.CreationTime -lt $Date){
       If ($PSCmdlet.ShouldProcess("Removing $($item_object.FullName)")) {
-        Remove-item $($item_object.FullName) -Force -Confirm:$false 
+        Remove-item $($item_object.FullName) -Force -Confirm:$false @args
         Write-Verbose "Removed $($item_object.FullName)"
       }
     }
