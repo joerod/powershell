d84ccd97a8e3ed9ec1e1a79da95f884047d33525
Joe Rodriguez <joerod@gmail.com>
2019-08-14 01:47:42 -0400

    Update ConvertFrom-DHCP.ps1

--- ConvertFrom-DHCP.ps1
+++ ConvertFrom-DHCP.ps1
@@ -17,26 +17,22 @@ Function ConvertFrom-DHCP {
     [string]$InterfaceAlias
   )
   $interface = Get-NetIPConfiguration -InterfaceAlias $InterfaceAlias
-  #Get ipv4 address
-  $ipaddress = $interface.ipv4address.ipv4address
-  #get default gateway
-  $ipgateway = $interface.ipv4defaultgateway.nexthop
-  #Getting the subnet mask
-  $mask = ((Get-NetIPAddress -InterfaceAlias $interface.InterfaceAlias).where{$_.AddressFamily -eq 'IPv4'}).PrefixLength
-  $cidr = (0..3|%{(,0*($_*8+1)+('Ààðøüþÿ'|% t*y|%{+$_})+,255*(24-$_*8))[$mask]})-join'.'
+  $dns = (Get-DnsClientServerAddress -InterfaceAlias 'vEthernet (external)' -AddressFamily IPv4).ServerAddresses
   #Output
   $new_ip_splat = @{
     interfacealias = $interfacealias
-    ipaddress      = $ipaddress
-    prefixlength   = $cidr
-    defaultgateway = $ipgateway
+    ipaddress      = $interface.ipv4address.ipv4address
+    defaultgateway = $interface.ipv4defaultgateway.nexthop
+    PrefixLength   = $(((Get-NetIPAddress -InterfaceAlias $interface.InterfaceAlias).where{$_.AddressFamily -eq 'IPv4'}).PrefixLength)
   }
 
   if ($PSCmdlet.ShouldProcess(($new_ip_splat.GetEnumerator() | ForEach-Object {"{0}`t{1}" -f $_.Name,($_.Value -join ", ")}))) {
-    Remove-NetIPAddress -InterfaceAlias $new_ip_splat.interfacealias -AddressFamily ipv4
+    Remove-NetIPAddress -InterfaceAlias $interfacealias -AddressFamily ipv4 | Out-Null
     New-NetIPAddress @new_ip_splat
+    Set-DnsClientServerAddress -InterfaceAlias $interfacealias -ServerAddresses $dns
   }
   else{
     exit
   }
+  
 }
