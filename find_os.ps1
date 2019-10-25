e587c32dd0bda286173d7f3d64505d7b465920b9
Joe Rodriguez <joerod@gmail.com>
2019-10-25 01:27:35 -0400

    Update find_os.ps1
    indentation , newer PSCustomObject

--- find_os.ps1
+++ find_os.ps1
@@ -1,17 +1,15 @@
 #this script takes a list of computers either IP or Name and shows what OS they are running. 
 
 Param(
-[Parameter(Position=0,mandatory=$true)]
-[array[]]$Machine
+  [Parameter(mandatory)]
+  [array[]]$Machine
 )
 
 foreach($x in $machine){
-
-(Get-WmiObject Win32_OperatingSystem -computer $x).caption | %{
-
-New-Object -TypeName PSCustomObject -Property @{
-    ComputerName = $x -join ''
-    "OS Version" = $_.caption -join ''
-   }
-    }  
-  } 
+  (Get-WmiObject Win32_OperatingSystem -computer $x).caption | foreach{
+    [PSCustomObject]@{
+      ComputerName = $x -join ''
+      "OS Version" = $_.caption -join ''
+    }
+  }  
+} 
