67b6a2654611282df83c9a753c96ad36602c6b59
Joe Rodriguez <joerod@gmail.com>
2019-10-25 01:50:02 -0400

    more functions and brevity
    remove unneeded parameter logic

--- vm_hdd_resize.ps1
+++ vm_hdd_resize.ps1
@@ -1,16 +1,18 @@
+  
 <#This script will resize a esx vmware host hdd then extend the hdd on the machine itself.  
   Prerequisits are Windows 7 and above and Windows 2008 and above, WinRM must also be enabled on the host machines. 
 #>
-
-Param(
-[Parameter(Position=0,mandatory=$true)]
-[string]$computername,
-[Parameter(Position=1,mandatory=$true)]
-[int]$newsize
-)
-
-$VMHDD = Get-HardDisk -vm $computername | ? {$_.Name -eq "hard disk 1"} 
-
-Set-HardDisk -harddisk $VMHDD -CapacityGB (([int]$vmhdd.CapacityGB) + ([int]$newsize)) -Confirm:$false
-
-Invoke-Command -ComputerName $computername -ScriptBlock {"rescan","select volume 2","extend" | diskpart}
+Function VMHDDResize {
+  Param(
+    [Parameter(mandatory)]
+    [string]$ComputerName,
+    [Parameter(mandatory)]
+    [int]$NewSize
+  )
+  
+  $VMHDD = Get-HardDisk -vm $ComputerName | where-object {$_.Name -eq "hard disk 1"} 
+  
+  Set-HardDisk -harddisk $VMHDD -CapacityGB (([int]$vmhdd.CapacityGB) + ([int]$newsize)) -Confirm:$false
+  
+  Invoke-Command -ComputerName $ComputerName -ScriptBlock {"rescan","select volume 2","extend" | diskpart}
+}
