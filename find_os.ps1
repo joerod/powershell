4b157a9632f3fe6f97a9710887e989d57986b9f8
Joe Rodriguez <joerod@gmail.com>
2019-10-25 01:33:13 -0400

    Update find_os.ps1
    functions are better

--- find_os.ps1
+++ find_os.ps1
@@ -1,15 +1,16 @@
 #this script takes a list of computers either IP or Name and shows what OS they are running. 
+function Find-OS {
+  Param(
+    [array[]]$ComputerName = $env:COMPUTERNAME
+  )
 
-Param(
-  [Parameter(mandatory)]
-  [array[]]$Machine
-)
+  foreach($x in $ComputerName){
 
-foreach($x in $machine){
-  (Get-WmiObject Win32_OperatingSystem -computer $x).caption | foreach{
-    [PSCustomObject]@{
-      ComputerName = $x -join ''
-      "OS Version" = $_.caption -join ''
-    }
-  }  
-} 
+    (Get-WmiObject Win32_OperatingSystem -computer $x).caption | foreach{
+      [PSCustomObject]@{
+        ComputerName = $x -join ''
+        "OS Version" = $_.caption -join ''
+      }
+    }  
+  } 
+}
