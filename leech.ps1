657307683a5fdedadd2a65ffaabc0c4aa6aeb65d
Joe Rodriguez <joerod@gmail.com>
2020-04-04 21:52:10 -0400

    Update leech.ps1
    updated qbitorrent and plex logic in leech script

--- leech.ps1
+++ leech.ps1
@@ -42,31 +42,31 @@ Function Get-qBtorrentInfo {
 }
 
 $Password = "" # API password for qbittorrent
-$token = "" # Token for Plex API
-$Username = 'admin' # user for qbittorrent
+$token = "" # Token for plex API
+$Username = 'admin' # password for qbittorrent
 $Port = '6969' # port for qbittorrent
-$plex,$Computer = '192.168.1.231' # address of plex server and where qbittorrent is running
+$plex = $Computer = '192.168.1.231' # address of plex server and where qbittorrent is running
 [int]$minutes = '1' # how often to check on downloading file(s)
 
 Connect-qBtorrent -Username $Username -Password $Password -Computer $Computer -Port $Port
 while ($results = Get-qBtorrentInfo -WebSession $session -Computer $Computer -Port $port) {
   foreach ($result in $results) {
     switch ($result.state) {
-      'uploading' {
+      {$_ -in 'uploading','stalledUP'} {
         Write-output "Removing $($result.name)"
         Remove-qBtorrent -WebSession $session -Hash $result.hash -Computer $Computer -Port $port
         # refresh plex if a movie is downloaded
         $return = Invoke-restmethod -uri "http://$($plex):32400/library/sections?X-Plex-Token=$($token)"
         foreach ($path in ($results.save_path | Select-Object -Unique)) {
           if($id = $return.MediaContainer.Directory.Location | Where-Object { ($_.path + "\") -eq $path}){
-            Invoke-restmethod -uri "http://$($plex):32400/library/sections/$($id.id)/refresh?X-Plex-Token=$($token)"
+            Invoke-restmethod -uri "http://$($plex):32400/library/sections/$($id.id)/refresh??force=1&X-Plex-Token=$($token)"
           }
           else{
-            Write-warning ("Could not find folder {0} to refresh in Plex" -f $id.path)
+            Write-warning ("Could not find folder {0} to refresh in Plex" -f ($($id.path) ? $($id.path) : "Null"))
           }
         }
       }
-      'default' {
+      default {
         Write-Warning ("Still leeching: {0} - {1}%" -f $($result.name), [math]::Round($($($result.progress) * 100), 2))
       }
     }
@@ -77,3 +77,4 @@ while ($results = Get-qBtorrentInfo -WebSession $session -Computer $Computer -Po
   }
   Start-Sleep ($minutes * 60)
 }
+Invoke-restmethod -uri "http://$($plex):32400/library/sections/4/refresh??force=1&X-Plex-Token=$($token)"
