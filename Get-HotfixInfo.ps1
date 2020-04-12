23247091b23a5cb1378cb9392e0fd340ec6d6388
Joe Rodriguez <joerod@gmail.com>
2020-04-12 01:31:59 -0400

    Update Get-HotfixInfo.ps1

--- Get-HotfixInfo.ps1
+++ Get-HotfixInfo.ps1
@@ -1,27 +1,5 @@
 # This script aims to give more detail than the Get-Hotfix.  In particular this shows install status and an accurate install date.  
-function # This script aims to give more detail than the Get-Hotfix.  In particular this shows install status and an accurate install date.  
 function Get-HotfixInfo {
-  $Session = New-Object -ComObject "Microsoft.Update.Session"
-  $Searcher = $Session.CreateUpdateSearcher()
-  $historycount = $Searcher.GetTotalHistoryCount()
-  $Searcher.QueryHistory(0, $historycount) | Select-Object Title, Description, Date,
-  @{ name = "Operation"; expression = { switch ($_.operation) {
-        1 { "Installation" }; 2 { "Uninstallation" }; 3 { "Other" };
-      }
-    }   
-  },
-  @{name = "Result"; expression = { switch ($_.ResultCode) {
-        0 { "Not Started" }; 1 { "In Progress" }; 2 { "Succeeded" }; 3 { "Succeeded With Errors" }; 4 { "Failed" }; 5 { "Aborted" }; default { "Unknown" } 
-      }
-    }
-  } ,
-  @{name = "HotFixID"; expression = { 
-      $_.title -match "\bKB[0-9]*\b" | Out-Null
-      $($Matches.Values)
-    }
-  } 
-}
-{
   $Session = New-Object -ComObject "Microsoft.Update.Session"
   $Searcher = $Session.CreateUpdateSearcher()
   $historycount = $Searcher.GetTotalHistoryCount()
