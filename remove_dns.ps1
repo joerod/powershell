a9f59094b045cd41341d0320ad504e9eb5423dcd
Joe Rodriguez <joerod@gmail.com>
2020-04-26 00:23:38 -0400

    Update remove_dns.ps1

--- remove_dns.ps1
+++ remove_dns.ps1
@@ -7,21 +7,21 @@
 #>
 
 
-foreach ($domainname in (Get-DnsServerZone -ComputerName crpnycdcrt01 | ? {($_.IsReverseLookupzone -like "false") -and ($_.ZoneType -like "Primary") } |select -ExpandProperty ZoneName)){
-function list{
+foreach ($domainname in (Get-DnsServerZone -ComputerName crpnycdcrt01 | ? { ($_.IsReverseLookupzone -like "false") -and ($_.ZoneType -like "Primary") } | Select-Object -ExpandProperty ZoneName)) {
+  function list {
 
-get-wmiobject -ComputerName dnsserver -Namespace root\microsoftDNS -Class MicrosoftDNS_ResourceRecord -Filter "domainname='$domainname'" |
- select IPAddress, ownername |
-  ? {($_.IPAddress -like '10.7.*') -or ($_.IPAddress -like '10.9.*') -and ($_.IPAddress -notlike $null)} |
-  Sort IPAddress #|
-  #export-csv C:\Users\jorodriguez\Desktop\$domainname.csv  -NoTypeInformation
+    get-wmiobject -ComputerName dnsserver -Namespace root\microsoftDNS -Class MicrosoftDNS_ResourceRecord -Filter "domainname='$domainname'" |
+    Select-Object IPAddress, ownername |
+    Where-Object { ($_.IPAddress -like '10.7.*') -or ($_.IPAddress -like '10.9.*') -and ($_.IPAddress -notlike $null) } |
+    Sort-Object IPAddress #|
+    #export-csv C:\Users\jorodriguez\Desktop\$domainname.csv  -NoTypeInformation
   }
 
 
-ForEach($ip in list){
-$ipnew = $ip.ownername -replace "\..+"
-#Remove-DnsServerResourceRecord -ZoneName "corporate.local" -RRType "A" -Name $($ip.ownername) -RecordData $($ip.IPAddress)
-dnscmd crpnycdcrt01 /RecordDelete $domainname $ipnew A $ip.IPAddress /f
-Write-output "$ipnew $($ip.IPAddress) has been removed from DNS" 
+  ForEach ($ip in list) {
+    $ipnew = $ip.ownername -replace "\..+"
+    #Remove-DnsServerResourceRecord -ZoneName "corporate.local" -RRType "A" -Name $($ip.ownername) -RecordData $($ip.IPAddress)
+    dnscmd crpnycdcrt01 /RecordDelete $domainname $ipnew A $ip.IPAddress /f
+    Write-output "$ipnew $($ip.IPAddress) has been removed from DNS" 
+  }
 }
- }
