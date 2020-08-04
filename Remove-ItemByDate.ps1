b0ac9600c3d22e4c289e6d9eaa8d27e694086125
Joe Rodriguez <joerod@gmail.com>
2020-08-03 22:14:14 -0400

    Update Remove-ItemByDate.ps1
    added the `literalpath` switch

--- Remove-ItemByDate.ps1
+++ Remove-ItemByDate.ps1
@@ -2,18 +2,13 @@ Function Remove-ItemByDate {
   <#
 .SYNOPSIS
 Removes a file or folder older than a specified date
-
 .DESCRIPTION
 Takes any strings for the file name or extension.
-
 .PARAMETER Item
 The file or folder to be compared to
-
 .PARAMETER Date
 The date the the file or folder must be younger than
-
 .EXAMPLE
-
 Remove-ItemByDate -Item D:\JoeRod\joerod\Desktop\movies -Date (Get-Date).AddMonths(-2)
 #>
   [CmdletBinding(SupportsShouldProcess=$True)]
@@ -22,6 +17,7 @@ Remove-ItemByDate -Item D:\JoeRod\joerod\Desktop\movies -Date (Get-Date).AddMont
   [datetime]$Date
   )
 
+  $args = @{}
 
   if((Get-item $item).psiscontainer) {
     $args.Recurse = $true
@@ -34,7 +30,7 @@ Remove-ItemByDate -Item D:\JoeRod\joerod\Desktop\movies -Date (Get-Date).AddMont
   foreach($item_object in (Get-ChildItem $Item @args)){
     if($item_object.CreationTime -lt $Date){
       If ($PSCmdlet.ShouldProcess("Removing $($item_object.FullName)")) {
-        Remove-item $($item_object.FullName) -Force -Confirm:$false @args
+        Remove-item -LiteralPath $($item_object.FullName) -Force -Confirm:$false @args
         Write-Verbose "Removed $($item_object.FullName)"
       }
     }
