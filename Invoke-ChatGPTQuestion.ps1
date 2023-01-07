716af635e507cfd00342361ac23d86fe47176a22
Joe Rodriguez <joerod@gmail.com>
2023-01-06 22:17:28 -0500

    Update Invoke-ChatGPTQuestion.ps1

--- Invoke-ChatGPTQuestion.ps1
+++ Invoke-ChatGPTQuestion.ps1
@@ -1,29 +1,32 @@
 Function Invoke-ChatGPTQuestion {
   [CmdletBinding()]
   param (
-      [string]$Question,
-      [int]$MaxTokens = 500,
-      [string]$APIKey
+    [string]$Question,
+    [int]$MaxTokens = 500,
+    [int]$Temperature = 0,
+    [string]$Model = "text-davinci-003",
+    [string]$APIKey
   )
-# Set up the request body
-$body = @{
-  'model'      = "text-davinci-003"
-  'prompt'     = $Question
-  "max_tokens" = $MaxTokens
-} | ConvertTo-Json
+  # Set up the request body
+  $body = @{
+    model       = $Model
+    prompt      = $Question
+    max_tokens  = $MaxTokens
+    temperature = $Temperature
 
-$splat = @{
-  Method      = 'Post'
-  Uri         = 'https://api.openai.com/v1/completions'
-  Body        = $body
-  ContentType = 'application/json'
-  Headers     = @{'Authorization' = "Bearer $api_key"}
-}
+  } | ConvertTo-Json
+
+  $splat = @{
+    Method      = 'Post'
+    Uri         = 'https://api.openai.com/v1/completions'
+    Body        = $body
+    ContentType = 'application/json'
+    Headers     = @{'Authorization' = "Bearer $api_key" }
+  }
   # Make the API call
-  $response   = Invoke-RestMethod @splat
+  $response = Invoke-RestMethod @splat
 
   # Process the response
   $response.choices.text
 }
-
 #Invoke-ChatGPTQuestion -Question "who owns the knicks" -APIKey $api_key 
