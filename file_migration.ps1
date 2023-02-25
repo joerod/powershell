ed38c3a734c09ca3a8f49622bd9477a66e605b19
Joe Rodriguez <joerod@gmail.com>
2023-02-25 01:43:19 -0500

    Update file_migration.ps1
    formatting

--- file_migration.ps1
+++ file_migration.ps1
@@ -9,34 +9,33 @@ if not  the folder is created and  the ACL are modified, Then the users AD profi
 If the folder already exists on server_a robocopy copies all the contents including file perms and sends a log everything
 #>
 
-foreach($user in (Get-Content C:\Users\admin\Desktop\userlist.txt)){
+foreach ($user in (Get-Content C:\Users\admin\Desktop\userlist.txt)) {
 
-#checks if user has a home directory if not it creates one on target server
-if((Test-Path -Path "\\server_a\d$\user\$user") -eq $false) {
-#creates new path since one does not exist
-$CreatePath = New-Item -Path "\\server_b\f$\UsersNY\$user" -itemtype directory
-$CreatePath = "\\server_b\f$\UserNew\$user"
+  #checks if user has a home directory if not it creates one on target server
+  if ((Test-Path -Path "\\server_a\d$\user\$user") -eq $false) {
+    #creates new path since one does not exist
+    $CreatePath = New-Item -Path "\\server_b\f$\UsersNY\$user" -itemtype directory
+    $CreatePath = "\\server_b\f$\UserNew\$user"
 
-#sets file permsion on new folder
-$acl = Get-Acl $CreatePath
-$permission = "contoso\$user","Modify", "ContainerInherit, ObjectInherit", "None", "Allow"
-$accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
-$acl.SetAccessRule($accessRule)
-$acl | Set-Acl $CreatePath
+    #sets file permsion on new folder
+    $acl = Get-Acl $CreatePath
+    $permission = "contoso\$user", "Modify", "ContainerInherit, ObjectInherit", "None", "Allow"
+    $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
+    $acl.SetAccessRule($accessRule)
+    $acl | Set-Acl $CreatePath
 
-#adds new home directory to AD
-$drive = "\\server_b\f$\UsersNew\"
-Set-ADUser $user -HomeDirectory ("$drive" + "$user") -HomeDrive U 
+    #adds new home directory to AD
+    $drive = "\\server_b\f$\UsersNew\"
+    Set-ADUser $user -HomeDirectory ("$drive" + "$user") -HomeDrive U 
 
   }
-  else
-  {
-#Copies data (only delta if file already exist)
-Send-mailmessage -to "Joe Rodriguez <joerod@companycom>" -From "Joe Rodriguez <joerod@companycom>" -Subject "Started Copying $User" -SmtpServer "mailserver.company.com"
-robocopy \\server_a\f$\UsersOld\$user \\server_b\f$\UsersNew\$user /e /COPYALL /xo /Z /R:1000000 /W:1 /LOG+:C:\robocopy.log /tee
-#Sets home drive in AD
-$drive = "\\contoso\dfs\UsersNew\"
-Set-ADUser $user -HomeDirectory ("$drive" + "$user") -HomeDrive U 
-Send-mailmessage -to "Joe Rodriguez <joerod@companycom>"" -From "Joe Rodriguez <joerod@companycom>"" -Subject "$User has been moved" -SmtpServer "mailserver.company.com"
+  else {
+    #Copies data (only delta if file already exist)
+    Send-mailmessage -to "Joe Rodriguez <joerod@companycom>" -From "Joe Rodriguez <joerod@companycom>" -Subject "Started Copying $User" -SmtpServer "mailserver.company.com"
+    robocopy \\server_a\f$\UsersOld\$user \\server_b\f$\UsersNew\$user /e /COPYALL /xo /Z /R:1000000 /W:1 /LOG+:C:\robocopy.log /tee
+    #Sets home drive in AD
+    $drive = "\\contoso\dfs\UsersNew\"
+    Set-ADUser $user -HomeDirectory ("$drive" + "$user") -HomeDrive U 
+    Send-mailmessage -to "Joe Rodriguez <joerod@companycom>"" -From "Joe Rodriguez <joerod@companycom>"" -Subject "$User has been moved" -SmtpServer "mailserver.company.com"
   }
 }
